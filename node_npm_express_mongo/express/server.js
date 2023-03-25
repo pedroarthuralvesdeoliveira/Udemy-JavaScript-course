d94df53a314736ea738fc30a799c8ccbfe4bbc4f
Lahat-Herev <pedroarthuralvesdeoliveira@hotmail.com>
2023-03-24 22:13:45 -0300

    routes

--- node_npm_express_mongo/express/server.js
+++ node_npm_express_mongo/express/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (request, response) => {
-    response.send("Hello, world!");
+/**
+ * Params: url/dado
+ * Query: url/?dado=valor&outrodado=outrovalor
+ * Body:  dados que vêm do formulário através de uma rota do tipo POST ou PUT
+ */
+
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/', (request, response) => {
+
 });
 
 app.get('/batata', (request, response) => {
     response.send("Bimda!");
+});
+
+app.get('/testes/:idUsuario?', (request, response) => {
+    console.log(request.params);
+    console.log(request.query);
+    response.send(request.params);
 });
 
 app.listen(3000, () => {
